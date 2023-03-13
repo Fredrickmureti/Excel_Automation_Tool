a6e0ec4d4ce58d5f0328ee89f9589f59f5654d08
Fredrickmureti <fredrickmureti612@gmail.com>
2023-03-13 18:19:36 +0300

    my second commit

--- spreadsheet_automation.py
+++ spreadsheet_automation.py
@@ -8,9 +8,9 @@ sheet = wb['python-spreadsheets.xlsx']
 for row in range(2, sheet.max_row + 1):
      cell = sheet.cell(row, column_reference)
 
-     values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=x, max_col=y)
+     values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=x, max_col=y) #in this case x and y represent the min and max colomns respectively
 
 chart = BarChart()
 chart.add_data(values)
-sheet.add_chart(BarChart, 'e2')
+sheet.add_chart(BarChart, 'e2') 
 wb.save('python-spreadsheet2.xlsx')
